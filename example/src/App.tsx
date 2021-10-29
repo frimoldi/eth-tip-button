ae3ae1ea3da4a5670290137a40173f51015f8570
Fran Rimoldi <francisco.rimoldi@gmail.com>
2021-10-29 18:17:54 -0300

    feat: basic button implementation with fixed addresses and fixed ETH

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'eth-tip-button'
+import { Button } from 'eth-tip-button'
 import 'eth-tip-button/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Button recipientAddress='frimoldi.eth'>Tip me some ETH!</Button>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { ethers } from 'ethers'
 import styles from './styles.module.css'
 
 interface Props {
@@ -7,4 +8,41 @@ interface Props {
 
 export const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
+}
+
+type ButtonProps = {
+  recipientAddress: string
+  children: React.ReactChild
+}
+
+export const Button = ({ recipientAddress, children }: ButtonProps) => {
+  // const [account, setAccount] = useState('')
+
+  const handleClick = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const accounts = await provider.listAccounts()
+
+    if (accounts.length === 0) {
+      await provider.send('eth_requestAccounts', [])
+    }
+
+    const signer = await provider.getSigner()
+    const address = await signer.getAddress()
+
+    console.log(`Connected to ${address}`)
+
+    const tx = await signer.sendTransaction({
+      to: '0x9b43748a60954F36A6547C5d653951532Af242e6',
+      value: ethers.utils.parseEther('0.001')
+    })
+
+    console.log(tx)
+
+    const receipt = await tx.wait()
+
+    console.log(receipt)
+  }
+
+  console.log(recipientAddress)
+  return <button onClick={handleClick}>{children}</button>
 }
